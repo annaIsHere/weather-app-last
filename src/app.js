23e006238c27b4ba8c9a5199ab207b28c8aeb1cc
annaIsHere <86635126+annaIsHere@users.noreply.github.com>
2021-08-05 17:21:14 -0600

    added search engine

--- src/app.js
+++ src/app.js
@@ -42,8 +42,18 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-let city = "osaka";
-let apiKey = "bf1a00186364b8a343b2844041168262";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "bf1a00186364b8a343b2844041168262";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(displayTemperature);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+search("new york");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
